1f49e65a789211eade87de73e313105193fcf6ef
David Runion <david.runion@imedemr.com>
2023-09-23 15:54:48 -0400

    weekly rewards, custom total reward for epoch, wallet filter

--- script.js
+++ script.js
@@ -5,15 +5,51 @@ document.addEventListener('DOMContentLoaded', function () {
     populateDateDropdown();
 });
 
+const filterInput = document.createElement('input');
+filterInput.id = 'wallet_filter';
+filterInput.type = 'text';
+filterInput.placeholder = 'Filter by wallet...';
+filterInput.addEventListener('input', () => {
+  const filterValue = filterInput.value.toLowerCase();
+  const rows = document.querySelectorAll('#dataTable tbody tr');
+
+  rows.forEach(row => {
+    const wallet = row.querySelector('td:first-child').textContent.toLowerCase();
+    if (wallet.includes(filterValue)) {
+      row.style.display = '';
+    } else {
+      row.style.display = 'none';
+    }
+  });
+});
+document.querySelector('#wallet_column').appendChild(filterInput);
+
+const totalVoiInput = document.querySelector('#totalVoi input');
+totalVoiInput.addEventListener('input', () => {
+  const newTotalVoi = Number(totalVoiInput.value);
+  const totalBlocks = Number(document.querySelector('#totalBlocks span').textContent.replace(',', ''));
+  const rows = document.querySelectorAll('#dataTable tbody tr');
+
+  rows.forEach(row => {
+    const blocksCell = row.querySelector('td:nth-child(3)');
+    const rewardsCell = row.querySelector('td:nth-child(2)');
+    const blocks = parseInt(blocksCell.textContent);
+    rewardsCell.textContent = blocks / totalBlocks * newTotalVoi / Math.pow(10,6);
+  });
+
+  //document.querySelector('#totalBlocks span').textContent = Math.round(newTotalVoi * totalBlocks / 50);
+});
+
 function populateDateDropdown() {
     // List all your CSV file dates here
-    const availableDates = ["2023-09-20", "2023-09-19"];
+    const availableDates = ["20230918-20230924"];
     const dropdown = document.getElementById('datePicker');
 
     availableDates.forEach(date => {
         const option = document.createElement('option');
         option.value = date;
-        option.textContent = date;
+        // convert `date` to human readable format
+        option.textContent = date.replace(/(\d{4})(\d{2})(\d{2})-(\d{4})(\d{2})(\d{2})/, '$1-$2-$3 to $4-$5-$6');
         dropdown.appendChild(option);
     });
 
@@ -30,7 +66,7 @@ function loadData() {
         .then(data => {
             const rows = data.split('\n').slice(1); // skip header row
             const dataArrays = rows.map(row => row.split(','));
-            dataArrays.sort((a, b) => Number(b[1]) - Number(a[1]));
+            dataArrays.sort((a, b) => Number(b[3]) - Number(a[3]));
 
             const tableBody = document.getElementById('dataTable').querySelector('tbody');
             tableBody.innerHTML = ''; // clear previous data
@@ -44,27 +80,30 @@ function loadData() {
 
                 const tr = document.createElement('tr');
 
-                columns.forEach((col, index) => {
-                    const td = document.createElement('td');
+                // Format first column
+                const td1 = document.createElement('td');
+                allAddresses.push(columns[0]);
+                td1.textContent = columns[0];
+                tr.appendChild(td1);
 
-                    // If it's the third column, add a percent symbol
-                    if (index === 2) {
-                        col += '%';
-                    }
-                    // If it's the second column, accumulate the total
-                    if (index === 1) {
-                        totalBlocks += parseInt(col);
-                    }
-                    if (index == 0) {
-                        allAddresses.push(col);
-                    }
-                    if (index === 3) {
-                        totalVoi += parseFloat(col);
-                    }
+                // Skip second column
 
-                    td.textContent = col;
-                    tr.appendChild(td);
-                });
+                // Format third column
+                const td3 = document.createElement('td');
+                totalVoi += Number(columns[2]);
+                td3.textContent = Math.round(columns[2] / Math.pow(10, 6) * 1000000) / 1000000;
+                tr.appendChild(td3);
+
+                // Format fourth column
+                const td4 = document.createElement('td');
+                totalBlocks += parseInt(columns[3]);
+                td4.textContent = columns[3];
+                tr.appendChild(td4);
+
+                // Format fifth column
+                const td5 = document.createElement('td');
+                td5.textContent = Math.round(columns[4] * 1000) / 10 + '%';
+                tr.appendChild(td5);
 
                 tableBody.appendChild(tr);
                 totalWallets++;
@@ -84,13 +123,11 @@ function loadData() {
             });
             
             
-            document.getElementById('totalBlocks').textContent = `Total blocks: ${totalBlocks}`;
-            document.getElementById('totalWallets').textContent = `Total wallets: ${totalWallets}`;
-            document.getElementById('totalVoi').textContent = `Total Voi: ${Math.round(totalVoi * Math.pow(10, 6)) / Math.pow(10, 6)}`;
-        });
+        document.querySelector('#totalBlocks span').textContent = totalBlocks;
+        document.querySelector('#totalWallets span').textContent = totalWallets;
+        document.querySelector('#totalVoi input').value = Math.round(totalVoi * Math.pow(10, 6)) / Math.pow(10, 6);
+    });
 }
-
-
 
 async function getNFD(data) {
     let addressChunks = [];
